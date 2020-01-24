bc627e4932a0cdcef50f1c5da3b9db3a661d7310
daikin555 <daaaakun@icloud.com>
2020-01-24 13:22:56 +0900

    update/task33

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -7,20 +7,25 @@ use Illuminate\Support\Facades\Auth;
 
 class RedirectIfAuthenticated
 {
-    /**
-     * Handle an incoming request.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure  $next
-     * @param  string|null  $guard
-     * @return mixed
-     */
-    public function handle($request, Closure $next, $guard = null)
-    {
-        if (Auth::guard($guard)->check()) {
-            return redirect('/home');
-        }
+	/**
+	 * Handle an incoming request.
+	 *
+	 * @param  \Illuminate\Http\Request  $request
+	 * @param  \Closure  $next
+	 * @param  string|null  $guard
+	 * @return mixed
+	 */
+	public function handle($request, Closure $next, $guard = null) {
+		$redirectTo = "/home";
 
-        return $next($request);
-    }
+		if ($guard === "admin") {
+			$redirectTo = "/admin/home";
+		}
+
+		if (Auth::guard($guard)->check()) {
+			return redirect('/home');
+		}
+
+		return $next($request);
+	}
 }
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,18 @@
 
 Route::get('/index', 'ItemController@index');
 Route::get('/detail/{id}', 'ItemController@detail')->name('items.item_name');
+
+Route::group(['prefix' => 'admin'], function() {
+	Route::get('/',         function () { return redirect('/admins/home'); });
+	//Route::get('/login',     'Admin\LoginController@LoginForm')->name('admins.login');
+	Route::post('/login',    'Admin\LoginController@login');
+});
+
+Route::group(['prefix' => 'admin', 'middleware' => 'auth:admins'], function() {
+	Route::post('logout',   'Admin\LoginController@logout')->name('admin.logout');
+	Route::get('home',      'Admin\HomeController@index')->name('admin.home');
+});
+
 /*Route::get('/', function () {
 	return view('welcome');
 });*/
