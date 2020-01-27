58d811b1dff67a640754ce004872406453923ae8
daikin555 <daaaakun@icloud.com>
2020-01-27 19:48:53 +0900

    task31

--- app/Http/Controllers/ItemController.php
+++ app/Http/Controllers/ItemController.php
@@ -1,18 +1,13 @@
 <?php
 
 namespace App\Http\Controllers;
-
 use DB;
 use Illuminate\Http\Request;
 
 class ItemController extends Controller {
 	public function index () {
-		$items = DB::table('items')->get();
-		return view('items.index', ['items' => $items]);
-	}
-	public function detail(Request $request, $id) {
-		$desc = DB::table('items')->where('id', '=', $id)->get();
-		return view('items.detail', ['item' => $desc]);
+		$var = 1;
+		return view('item.index', ['var' => $var]);
 	}
 
 }
