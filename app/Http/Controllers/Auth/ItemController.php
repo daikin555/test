6d6ae8042cca29e1213c165f2b67c8e4efd17005
daikin555 <daaaakun@icloud.com>
2020-01-27 17:02:00 +0900

    update

---
+++ app/Http/Controllers/Auth/ItemController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+use DB;
+use Illuminate\Http\Request;
+
+class ItemController extends Controller {
+	public function index () {
+		$num = 1;
+		dd($num);
+		$items = DB::table('items')->get();
+		return view('items.index', ['items' => $items]);
+	}
+
+	public function detail(Request $request, $id) {
+		$desc = DB::table('items')->where('id', '=', $id)->get();
+		return view('items.detail', ['item' => $desc]);
+	}
+}
