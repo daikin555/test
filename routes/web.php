f85f57e74326e4ea3c34ea81664c2ce590ccd2cd
daikin555 <daaaakun@icloud.com>
2020-02-02 16:14:05 +0900

    update

--- routes/web.php
+++ routes/web.php
@@ -16,19 +16,19 @@ Route::group(['prefix' => 'admin', 'middleware' => 'auth:user'], function() {
 	Route::get('home', 'HomeController@index')->name('home');
 });
 
-	Route::group(['prefix' => 'admin'], function() {
-		Route::get('/', function () { return redirect('/index'); });
-		Route::get('/index', 'Admin\ItemController@index')->name('items.index');
-		Route::get('login', 'Admin\LoginController@indexLoginForm')->name('admins.login');
-		Route::get('/detail/{id}', 'Admin\ItemController@detail')->name('items.name');
-		Route::get('/edit', 'Admin\ItemController@edit')->name('items.edit');
-		Route::post('/edit', 'Admin\ItemController@update')->name('items.edit');
-		Route::post('login', 'Admin\LoginController@login');
-		Route::post('/update', 'Admin\ItemController@add')->name('items.add');
-		Route::get('/items/update', 'Admin\ItemController@create')->name('items.update');
-	});
+Route::group(['prefix' => 'admin'], function() {
+	Route::get('/', function () { return redirect('/index'); });
+	Route::get('/index', 'Admin\ItemController@index')->name('items.index');
+	Route::get('login', 'Admin\LoginController@indexLoginForm')->name('admins.login');
+	Route::get('/detail/{id}', 'Admin\ItemController@detail')->name('items.name');
+	Route::get('/edit', 'Admin\ItemController@edit')->name('items.edit');
+	Route::post('/edit', 'Admin\ItemController@update')->name('items.edit');
+	Route::post('login', 'Admin\LoginController@login');
+	Route::post('/update', 'Admin\ItemController@add')->name('items.add');
+	Route::get('/items/update', 'Admin\ItemController@create')->name('items.update');
+});
 
-	Route::group(['prefix' => 'admin', 'middleware' => 'auth:admin'], function() {
-		Route::post('logout', 'Admin\LoginController@logout')->name('admins.logout');
-		Route::get('home', 'Admin\HomeController@index')->name('admins.home');
-	});
+Route::group(['prefix' => 'admin', 'middleware' => 'auth:admin'], function() {
+	Route::post('logout', 'Admin\LoginController@logout')->name('admins.logout');
+	Route::get('home', 'Admin\HomeController@index')->name('admins.home');
+});
--- storage/framework/views/39ff09163a125e20839e90ad77076b768783bad1.php
+++ storage/framework/views/39ff09163a125e20839e90ad77076b768783bad1.php
@@ -34,7 +34,7 @@
 	<td>
 	<?php if($item->stock == 0): ?>
 		在庫なし
-	<?php elseif($item->stock >= 1): ?>
+	<?php else: ?>
 		在庫あり
 	<?php endif; ?>
 	</td>
